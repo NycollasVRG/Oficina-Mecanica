388f884c946057deb30da3b34f45fc6e2a9bd675
khauee <khauebraga55@gmail.com>
2026-01-28 18:16:21 -0300

    Ajuste final

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Sistema de Gerenciamento de Oficina Mecânica iniciado com sucesso!");
+        System.out.println("Lembrando da branch!");
 
     }
 }
